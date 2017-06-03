1b061650f9f22beb64ce35407089ce18f4049004
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2017-06-03 13:17:07 +0200

    [Color] Add custom colors

--- app/src/main/java/fr/tvbarthel/apps/shapi/shape/Rectangle.java
+++ app/src/main/java/fr/tvbarthel/apps/shapi/shape/Rectangle.java
@@ -10,12 +10,15 @@ import android.graphics.RectF;
 public class Rectangle implements Shape {
 
     @Override
-    public void draw(Canvas canvas, RectF rect, Paint paint) {
+    public void draw(Canvas canvas, RectF rect, Paint paint, Paint borderPaint) {
         if (rect.width() == rect.height()) {
             canvas.drawRect(rect.left, (rect.top + rect.centerY()) / 2f, rect.right,
                     (rect.bottom + rect.centerY()) / 2f, paint);
+            canvas.drawRect(rect.left, (rect.top + rect.centerY()) / 2f, rect.right,
+                    (rect.bottom + rect.centerY()) / 2f, borderPaint);
         } else {
             canvas.drawRect(rect.left, rect.top, rect.right, rect.bottom, paint);
+            canvas.drawRect(rect.left, rect.top, rect.right, rect.bottom, borderPaint);
         }
     }
 }
