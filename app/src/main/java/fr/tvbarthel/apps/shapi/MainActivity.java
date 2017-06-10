1bc3384feafc43478c3dfc0934e6f8da35a0321e
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2017-06-10 11:35:41 +0200

    [Main] :fire: Remove unused import

--- app/src/main/java/fr/tvbarthel/apps/shapi/MainActivity.java
+++ app/src/main/java/fr/tvbarthel/apps/shapi/MainActivity.java
@@ -5,7 +5,6 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.TextView;
 
 import javax.inject.Inject;
 
